b2898f7aad0a987c8168d82bc429693ea527fb49
qoolander <sam@toth.co.uk>
2020-03-31 18:01:40 +0100

    Add to Version Control

---
+++ src/Note.h
@@ -0,0 +1,27 @@
+//
+// Created by Sam on 08/03/2020.
+//
+
+#ifndef TRADE_NOTE_H
+#define TRADE_NOTE_H
+
+#include <string>
+#include <sstream>
+#include <cmath>
+
+#define SSTR( x ) static_cast< std::ostringstream & >( \
+        ( std::ostringstream() << std::dec << x ) ).str()
+
+const char Notes[12][3] = { "C", "C#", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B" };
+
+std::string getNoteName(int freq, int *midiCentDiff) {
+    int a = 440; //frequency of A (common value is 440Hz)
+    float midic = (12 * std::log2((float)freq /(float)a)) + 69; //A4 is midi 69
+    *midiCentDiff = roundf((-roundf(midic)+midic)*100);
+    int midi = round(midic);
+    int octave = (midi / 12) - 1;
+    return std::string(Notes[midi%12]) + SSTR(octave);
+}
+
+
+#endif //TRADE_NOTE_H
